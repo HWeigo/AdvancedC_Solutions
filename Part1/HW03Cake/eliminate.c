11ba3bbc7bcbd9e08c88f9287bd474b53d5952a8
HWeigo <746144870@qq.com>
2019-09-01 23:16:50 -0400

    completed requirement, but got error of Makefile

--- Part1/HW03Cake/eliminate.c
+++ Part1/HW03Cake/eliminate.c
@@ -23,34 +23,77 @@ void eliminate(int n, int k)
       return;
     }
 
-#ifdef DEBUG
-
+#ifdef DEBUG 
 	fprintf(stdout, "arr has %d number. \n", n);
 	fprintf(stdout, "k is  %d. \n", k);
-
 #endif
  
 	// initialize all elements
 	for(int i=0; i< n; ++i)
 	{
-		arr[i] = i + 1;
+		arr[i] = i;
+
+#ifdef DEBUG 
 		fprintf(stdout, "%d \n", arr[i]);
+#endif 
+
 	}
-  
-  // counting to k,
-  // mark the eliminated element
-  // print the index of the marked element
-  // repeat until only one element is unmarked
+#ifdef DEBUG 
+	fprintf(stdout,"\n \n");
+#endif 
 
+	// counting to k,
+	// mark the eliminated element
+	// print the index of the marked element
+	// repeat until only one element is unmarked
+	int idx = 0;
+	int remains_num = n;
 
+	while(remains_num != 1)
+	{
+		// counting to k
+		int set_idx = 0,	// flag used to detect whether idx reach k
+			cnt = 1;		// counter of how many valid step was taken
+		while(set_idx != 1)
+		{
+			idx++;
+			if(idx == n) idx = 0;
+			if(arr[idx] != -1)
+			{
+				cnt++;
+			}
+			if(cnt == k) set_idx = 1;
+		}
+		// fprintf(stdout, "idx: %d \n", idx);
+		fprintf(stdout, "%d\n", arr[idx]);
+		arr[idx] = -1;
+		
+#ifdef DEBUG
+		for(int i=0; i<n; ++i)
+		{
+			fprintf(stdout, "%d \t", arr[i]);
+		}
+		fprintf(stdout,"\n");
+#endif
 
+#ifdef DEBUG 
+		fprintf(stdout, "Before eliminate, the idx is at: %d \n", idx);
+#endif 
+		do
+		{
+			idx++;
+			if(idx == n) idx = 0;
+		}while(arr[idx] == -1);
+#ifdef DEBUG 
+		fprintf(stdout, "After eliminate, the idx is at: %d \n", idx);
+#endif 
+		remains_num--;
+	}
+	
+	// print the last one
+	fprintf(stdout, "%d \n", arr[idx]);
 
-  // print the last one
-
-
-
-
-  // release the memory of the array
-  free (arr);
+	// release the memory of the array
+	free (arr);
 }
 #endif
