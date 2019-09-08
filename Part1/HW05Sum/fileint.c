dbc435f05b2333b8dcccdc3923d5d678fcefffdf
HWeigo <746144870@qq.com>
2019-09-08 16:53:38 -0400

    update hw05, pass all the tests

--- Part1/HW05Sum/fileint.c
+++ Part1/HW05Sum/fileint.c
@@ -6,11 +6,46 @@
 #include <stdbool.h>
 
 #ifdef TEST_ADDFILE
+
+// #define DEBUG 
 bool addFile(char * filename, int * sum)
 {
+	
   // You cannot assume * sum is zero. Thus, * sum needs to be set 0
+	
+	*sum = 0;
+#ifdef DEBUG
+	fprintf(stdout, "sum = %d\n", *sum);
+#endif 
   // open a file whose name is filename for reading
   // if fopen fails, return false. Do NOT fclose
+	FILE *fptr;
+	fptr = fopen(filename, "r");
+	if(fptr == NULL)
+	{
+		fprintf(stderr, "fopen failed.\n");
+		return false;
+	}
+#ifdef DEBUG
+	fprintf(stdout, "fopen succeed.\n");
+#endif
+
+	int var;
+	do 
+	{
+		fscanf(fptr, "%d", &var);
+		if(!feof(fptr))
+		{
+#ifdef DEBUG 
+			fprintf(stdout, "var = %d\n", var);
+#endif
+			*sum += var;
+		}
+	}while(!feof(fptr));
+#ifdef DEBUG 
+	fprintf(stdout, "sum = %d\n", *sum);
+#endif
+	fclose(fptr);
   // if fopen succeeds, read integers using fscan (do not use fgetc)
   //
   // * sum stores the result of adding all numbers from the file
@@ -22,6 +57,7 @@ bool addFile(char * filename, int * sum)
 
 
 #ifdef TEST_WRITESUM
+// #define DEBUG 
 bool writeSum(char * filename, int sum)
 {
   // open a file whose name is filename for writing
@@ -29,6 +65,20 @@ bool writeSum(char * filename, int sum)
   // fprintf should use one newline '\n'
   // fclose, return true
   //
+	FILE *fptr;
+	fptr = fopen(filename, "w");
+	if(fptr == NULL)
+	{
+		fprintf(stderr, "fopen failed.\n");
+		return false;
+	}
+#ifdef DEBUG
+	fprintf(stdout, "fopen succeed.\n");
+#endif
+
+	fprintf(fptr, "%d\n", sum);
+	
+	fclose(fptr);
   return true;
 }
 #endif
