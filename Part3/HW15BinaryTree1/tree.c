0ff2fd4e4007ee0404c7f4b2e6af553239d60210
HWeigo <746144870@qq.com>
2019-11-13 11:46:00 +0800

    add makefile

---
+++ Part3/HW15BinaryTree1/tree.c
@@ -0,0 +1,114 @@
+// ***
+// *** You MUST modify this file
+// ***
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tree.h"
+
+// DO NOT MODIFY FROM HERE --->>>
+void deleteTreeNode(TreeNode * tr)
+{
+  if (tr == NULL)
+    {
+      return;
+    }
+  deleteTreeNode (tr -> left);
+  deleteTreeNode (tr -> right);
+  free (tr);
+}
+
+void freeTree(Tree * tr)
+{
+  if (tr == NULL)
+    {
+      // nothing to delete
+      return;
+    }
+  deleteTreeNode (tr -> root);
+  free (tr);
+}
+
+static void preOrderNode(TreeNode * tn, FILE * fptr)
+{
+  if (tn == NULL)
+    {
+      return;
+    }
+  fprintf(fptr, "%d\n", tn -> value);
+  preOrderNode(tn -> left, fptr);
+  preOrderNode(tn -> right, fptr);
+}
+
+void preOrder(Tree * tr, char * filename)
+{
+  if (tr == NULL)
+    {
+      return;
+    }
+  FILE * fptr = fopen(filename, "w");
+  preOrderNode(tr -> root, fptr);
+  fclose (fptr);
+}
+// <<<--- UNTIL HERE
+
+// ***
+// *** You MUST modify the follow function
+// ***
+
+#ifdef TEST_BUILDTREE
+// Consider the algorithm posted on
+// https://www.geeksforgeeks.org/construct-a-binary-tree-from-postorder-and-inorder/
+TreeNode *newNode(int value)
+{
+	TreeNode *node = malloc(sizeof(TreeNode));
+	node->value = value;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+
+int search(int *inArray, int inStrt, int inEnd, int value)
+{
+	for(int i= inStrt; i<=inEnd;++i)
+	{
+		if(inArray[i] == value)
+		{
+			return i;
+			break;
+		}
+	}
+	fprintf(stderr, "Do not find the value in inArray.\n");
+	return 0;
+}
+
+Tree * buildTreeHelp(int *inArray, int *postArray, int inStrt, int inEnd, int *pIndex)
+{
+	if(inStrt > inEnd)
+	{
+		return NULL;
+	}
+
+	TreeNode *node = newNode(postArray[*pIndex]);
+	(*pIndex)--;
+
+	if(inStrt == inEnd)
+	{
+		return node;
+	}
+
+	int iIndex = search(inArray, inStrt, inEnd, node->value);
+
+	buildTreeHelp(inArray, postArray, iIndex + 1, inEnd, &pIndex);
+	buildTreeHelp(inArray, postArray, inStrt, iIndex - 1, &pIndex);
+
+	return node;
+}
+
+Tree * buildTree(int * inArray, int * postArray, int size)
+{
+	int pIndex = size - 1;
+	return buildTreeHelp(inArray, postArray, 0, n-1, &pIndex);
+}
+#endif
