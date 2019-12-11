1c908f0ff20996bda2542aa12d3308d94624e3e8
HWeigo <746144870@qq.com>
2019-12-12 07:08:50 +0800

    finish hw18

--- Part4/HW18Memory/main.c
+++ Part4/HW18Memory/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "memory.h"
 
 void printOccupancy(Memory *mem)
@@ -12,6 +13,63 @@ void printOccupancy(Memory *mem)
 	}
 	printf("\n");
 }
+
+const int Operations[] = {'+', '-'}; 
+
+int isOperator(char * word) 
+{ 
+	int ind; 
+	int numop = sizeof(Operations) / sizeof(int); 
+
+	for (ind = 0; ind < numop; ind ++) 
+	{ 
+
+		char *loc = strchr(word, Operations[ind]); 
+		if (loc != NULL && !isdigit(loc[1])) 
+		{ 
+			return ind; 
+        } 
+    } 
+	return -1; 
+}
+
+bool memoryHelp(char *filename, Memory *mem)
+{
+	FILE *fptr = NULL;
+	fptr = fopen(filename, "r");
+	if(fptr == NULL)
+	{
+		return false;
+	}
+
+	char input[10];
+	int size = 0;
+	while(fgets(input, 10, fptr) != NULL)
+	{
+		switch(isOperator(input))
+		{
+			case 0:
+				if(fgets(input, 10, fptr) == NULL)
+				{
+					return false;
+				}
+				size = strtol(input, NULL, 10);
+				allocateMemory(mem, size);
+				break;
+			case 1:
+				if(fgets(input, 10, fptr) == NULL)
+				{
+					return false;
+				}
+				size = strtol(input, NULL, 10);
+				freeMemory(mem, size);
+				break;
+		}
+	}
+	fclose(fptr);
+	return true;
+}
+
 int main(int argc, char ** argv)
 {
 	// argv[1]: size of memory
@@ -22,13 +80,15 @@ int main(int argc, char ** argv)
 		return EXIT_FAILURE;
 	}
 	int totalSize = (int)strtol(argv[1], NULL, 10);
-	int alloSize = (int)strtol(argv[2], NULL, 10);
 	Memory *mem = NULL;
-	createMemory(&mem, totalSize);
-	allocateMemory(mem, alloSize);
-	allocateMemory(mem, 2);
-	freeMemory(mem, 0);
-	allocateMemory(mem,3);
+	if(!createMemory(&mem, totalSize))
+	{
+		return EXIT_FAILURE;
+	}
+	if(!memoryHelp(argv[2], mem))
+	{
+		return EXIT_FAILURE;
+	}
 	saveOccupancy(mem, argv[3]);
 	printOccupancy(mem);
 	destroyMemory(mem);
